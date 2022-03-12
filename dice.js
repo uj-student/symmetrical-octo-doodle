922c9644a003bec23b9ccbb900b8f28aa097a0fe
mukuru-bless <blessing.chiura@mukuru.com>
2022-03-12 07:31:15 +0200

    Initial Commit

---
+++ dice.js
@@ -0,0 +1,121 @@
+let die1Wins = 0;
+let die2Wins = 0;
+let draws = 0;
+
+function setupPage() {
+    let imageElement = document.createElement("img");
+    imageElement.setAttribute("src", "../image-asserts/dice.jpeg");
+    imageElement.setAttribute("id", "dicePlaceHolderImage")
+    document.getElementById("container").appendChild(imageElement)
+}
+
+function loadDiceImages() {
+    let die1Container = document.createElement("div")
+    die1Container.setAttribute("class", "diceImage")
+    die1Container.setAttribute("id", "diceImage")
+    let dieImage1 = document.createElement("img")
+    dieImage1.setAttribute("id", "firstDieImg")
+    dieImage1.setAttribute("alt", "First Die Image")
+    document.getElementById("container").appendChild(die1Container)
+    document.getElementById("diceImage").appendChild(dieImage1)
+
+    let die2Container = document.createElement("div")
+    die2Container.setAttribute("class", "diceImage")
+    die2Container.setAttribute("id", "diceImage")
+    let dieImage2 = document.createElement("img")
+    dieImage2.setAttribute("id", "secondDieImg")
+    dieImage2.setAttribute("alt", "Second Die Image")
+    document.getElementById("container").appendChild(die2Container)
+    document.getElementById("diceImage").appendChild(dieImage2)
+}
+
+function cleanupPage() {
+    if (document.getElementById("dicePlaceHolderImage") != null) {
+        document.getElementById("dicePlaceHolderImage").remove()
+        loadDiceImages()
+    }
+}
+
+function setScore(valueToUpdate) {
+    if (valueToUpdate === "die1Wins") {
+        die1Wins++;
+    } else if (valueToUpdate === "die2Wins") {
+        die2Wins++;
+    } else {
+        draws++;
+    }
+}
+
+function getScore() {
+    return `Die 1 has -> ${die1Wins} wins. \nDie 2 has -> ${die2Wins} wins. Number of draws -> ${draws}`;
+}
+
+function changeDiceBackground(die1Value, die2Value) {
+    if (die1Value > die2Value) {
+        document.getElementById("firstDieImg").style.background = "green";
+        document.getElementById("secondDieImg").style.background = "red";
+        setScore("die1Wins");
+    } else if (die1Value < die2Value) {
+        document.getElementById("secondDieImg").style.background = "green";
+        document.getElementById("firstDieImg").style.background = "red";
+        setScore("die2Wins");
+    } else {
+        document.getElementById("secondDieImg").style.background = "orange";
+        document.getElementById("firstDieImg").style.background = "orange";
+        setScore("draws");
+    }
+}
+
+function rollADie() {
+    return Math.ceil(Math.random() * 6);
+}
+
+function rollSomeDice() {
+    const die1Value = rollADie();
+    const die2Value = rollADie();
+
+    cleanupPage()
+
+    if (die1Value === 1) {
+        document.getElementById("firstDieImg").src = "../image-asserts/1.png";
+    }
+    if (die2Value === 1) {
+        document.getElementById("secondDieImg").src = "../image-asserts/1.png";
+    }
+    if (die1Value === 2) {
+        document.getElementById("firstDieImg").src = "../image-asserts/2.png";
+    }
+    if (die2Value === 2) {
+        document.getElementById("secondDieImg").src = "../image-asserts/2.png";
+    }
+    if (die1Value === 3) {
+        document.getElementById("firstDieImg").src = "../image-asserts/3.png";
+    }
+    if (die2Value === 3) {
+        document.getElementById("secondDieImg").src = "../image-asserts/3.png";
+    }
+    if (die1Value === 4) {
+        document.getElementById("firstDieImg").src = "../image-asserts/4.png";
+    }
+    if (die2Value === 4) {
+        document.getElementById("secondDieImg").src = "../image-asserts/4.png";
+    }
+    if (die1Value === 5) {
+        document.getElementById("firstDieImg").src = "../image-asserts/5.png";
+    }
+    if (die2Value === 5) {
+        document.getElementById("secondDieImg").src = "../image-asserts/5.png";
+    }
+    if (die1Value === 6) {
+        document.getElementById("firstDieImg").src = "../image-asserts/6.png";
+    }
+    if (die2Value === 6) {
+        document.getElementById("secondDieImg").src = "../image-asserts/6.png";
+    }
+    afterRoll(die1Value, die2Value)
+}
+
+function afterRoll(die1Value, die2Value) {
+    changeDiceBackground(die1Value, die2Value);
+    document.getElementById("results").innerHTML = getScore();
+}
---
+++ table.js
@@ -0,0 +1,29 @@
+function getAgeAge() {
+    const ageColumn = document.getElementsByClassName("ageField")
+    let totalAge = 0;
+    for (let index = 0; index < ageColumn.length; index++) {
+        totalAge = totalAge + parseInt(ageColumn.item(index).innerHTML);
+    }
+    return `The average age of the people in the table is: \n${totalAge / ageColumn.length}`;
+}
+
+function getNumberOfMarriedPeople() {
+    const marriageColumn = document.getElementsByClassName("marriageField");
+    let numberOfMarried = 0;
+    for (let index = 0; index < marriageColumn.length; index++) {
+        if (marriageColumn[index].innerHTML === "Yes") {
+            numberOfMarried++;
+        }
+    }
+    return `The table has ${numberOfMarried} married people.`;
+}
+
+function showAlert(idElement, func) {
+    let pressedButton = document.getElementById(idElement)
+    pressedButton.addEventListener("click", function (event) {
+                alert(func)
+            }//, {once: true}
+    )
+}
+
+// look into first click not working. Probably browser related.
